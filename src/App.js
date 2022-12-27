ad20626c3cc7910eda4aef133daf2da8336a38d7
Chris Barber-Riley <chris.barberriley@gmail.com>
2022-12-27 16:43:39 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currentCountry, setCurrentCountry] = useState('');
+  const [questionOptions, setQuestionOptions] = useState([]);
+  const [score, setScore] = useState(0);
+  const [questionsAnswered, setQuestionsAnswered] = useState(0);
+  const answerLength = 6;
+  const [correctAnswer, setCorrectAnswer] = useState(false);
+
+  const getCountries = () => {
+    fetch('assets/world.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        const rand = getRandomCountry(myJson);
+        console.log(rand);
+        setCurrentCountry(myJson[rand]);
+        console.log('Current Country: ', currentCountry);
+        const options = [];
+        options.push(myJson[rand]);
+        for (let index = 0; index < answerLength - 1; index++) {
+          options.push(myJson[getRandomCountry(myJson)]);
+        }
+        let shuffled = options
+          .map((value) => ({ value, sort: Math.random() }))
+          .sort((a, b) => a.sort - b.sort)
+          .map(({ value }) => value);
+        setQuestionOptions(shuffled);
+      });
+  };
+
+  const getRandomCountry = (countries, excludeId) => {
+    return Math.round(Math.random() * (countries.length - 1));
+  };
+
+  const checkAnswer = (answer) => {
+    return () => {
+      if (answer === currentCountry.alpha2) {
+        // alert('Correct!');
+        setCorrectAnswer(true);
+        setScore(score + 1);
+        setQuestionsAnswered(questionsAnswered + 1);
+        console.log('Score', score);
+        console.log('Questions Answered', questionsAnswered);
+      } else {
+        // alert('Wrong! it was ' + currentCountry.en + '!');
+        setCorrectAnswer(false);
+        setQuestionsAnswered(questionsAnswered + 1);
+        console.log('Questions Answered', questionsAnswered);
+      }
+      getCountries();
+    };
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <p>Current Country: {currentCountry.en}</p> */}
+        {questionsAnswered > 0 && (
+          <p>{correctAnswer ? <>Correct</> : <>Wrong</>}</p>
+        )}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Score: {score} : {questionsAnswered} (questions answered)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={`assets/128x128/${currentCountry.alpha2}.png`} alt='logo' />
+
+        <div className='optionsWrapper'>
+          {questionOptions.map((option) => {
+            return (
+              <button className='App-link' onClick={checkAnswer(option.alpha2)}>
+                {option.en}
+              </button>
+            );
+          })}
+        </div>
       </header>
     </div>
   );
